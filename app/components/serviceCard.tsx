0379c03994172cfafc17186a7b63e91874ea3901
Marjorie-Pereira <marjorie300904@gmail.com>
2026-05-29 23:03:03 -0300

    advertisement section

---
+++ app/components/serviceCard.tsx
@@ -0,0 +1,22 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { LucideIcon } from "lucide-react";
+
+type ServiceCardProps = {
+  Icon: LucideIcon;
+  title: string
+  description: string
+};
+export default function ServiceCard({ Icon, title, description }: ServiceCardProps) {
+  return (
+    <Card size="sm" className="min-w-60 max-w-80 flex flex-col items-center ">
+      <CardContent className="py-6">
+        <div className="bg-blue-100 rounded-full flex justify-center items-center w-fit p-4 mx-auto mb-6">
+          <Icon className="text-blue-500" size={32} />
+        </div>
+        <h3 className="text-2xl font-semibold mb-6">{title}
+        </h3>
+        <p className="text-slate-600">{description}</p>
+      </CardContent>
+    </Card>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,12 @@ import {
   SelectGroup,
   SelectItem,
 } from "@/components/ui/select";
-import { Search } from "lucide-react";
+import { Building, House, MapPin, Search } from "lucide-react";
 import Image from "next/image";
 import { SearchProperty } from "./components/searchProperty";
 import PropertyCard from "./components/propertyCard";
 import Link from "next/link";
+import ServiceCard from "./components/serviceCard";
 
 export default function Home() {
   return (
@@ -44,19 +45,78 @@ export default function Home() {
           Selecionamos as melhores oportunidades para você
         </p>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          <PropertyCard bathrooms={3} bedrooms={3} image="/casademo.jpeg" location="Capão da Canoa, RS" title="Casa linda" price={500000} type="venda" area={200}  />
-          <PropertyCard bathrooms={3} bedrooms={3} image="/casademo.jpeg" location="Capão da Canoa, RS" title="Casa linda" price={500000} type="aluguel" area={200}  />
-          <PropertyCard bathrooms={3} bedrooms={3} image="/casademo.jpeg" location="Capão da Canoa, RS" title="Casa linda" price={500000} type="venda" area={200}  />
-          <PropertyCard bathrooms={3} bedrooms={3} image="/casademo.jpeg" location="Capão da Canoa, RS" title="Casa linda" price={500000} type="venda" area={200}  />
+          <PropertyCard
+            bathrooms={3}
+            bedrooms={3}
+            image="/casademo.jpeg"
+            location="Capão da Canoa, RS"
+            title="Casa linda"
+            price={500000}
+            type="venda"
+            area={200}
+          />
+          <PropertyCard
+            bathrooms={3}
+            bedrooms={3}
+            image="/casademo.jpeg"
+            location="Capão da Canoa, RS"
+            title="Casa linda"
+            price={500000}
+            type="aluguel"
+            area={200}
+          />
+          <PropertyCard
+            bathrooms={3}
+            bedrooms={3}
+            image="/casademo.jpeg"
+            location="Capão da Canoa, RS"
+            title="Casa linda"
+            price={500000}
+            type="venda"
+            area={200}
+          />
+          <PropertyCard
+            bathrooms={3}
+            bedrooms={3}
+            image="/casademo.jpeg"
+            location="Capão da Canoa, RS"
+            title="Casa linda"
+            price={500000}
+            type="venda"
+            area={200}
+          />
         </div>
         <div className="pt-10">
           <Button
             variant={"outline"}
             className="py-4 px-6 border-blue-500 text-blue-500"
           >
-            <Link href={'/imoveis'}>VER TODOS OS IMÓVEIS</Link>
-            
+            <Link href={"/imoveis"}>VER TODOS OS IMÓVEIS</Link>
           </Button>
+        </div>
+      </section>
+      {/* serviços */}
+      <section className="mx-auto py-18 px-8 text-center bg-slate-100 h-screen">
+        <h2 className="text-3xl font-semibold mb-0">Nossos Serviços</h2>
+        <p className="text-xl mt-4 mb-10 text-slate-600">
+          Soluções completas para suas necessidades imobiliárias
+        </p>
+        <div className="flex justify-center gap-8">
+          <ServiceCard
+            Icon={House}
+            title="Compra e Venda"
+            description="Encontre o imóvel perfeito ou venda o seu com nossa expertise"
+          />
+          <ServiceCard
+            Icon={Building}
+            title="Locação"
+            description="Casas e apartamentos para alugar nas melhores regiões"
+          />
+          <ServiceCard
+            Icon={MapPin}
+            title="Consultoria"
+            description="Orientação especializada em investimentos imobiliários"
+          />
         </div>
       </section>
     </main>
